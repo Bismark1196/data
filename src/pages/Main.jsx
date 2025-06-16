c56fa25a86f82ea2694e2756afa3418713f85484
kipkoechkelvin <mutaikevin41@gmail.com>
2025-06-16 14:32:00 +0300

    Fix: add react-helmet-async dependency and update Main.jsx

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,8 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/Home.css";
 import App from "./pages/Home.jsx";
-import { HelmetProvider } from "react-helmet-async";
-import { Helmet } from 'react-helmet-async';
 
 
 createRoot(document.getElementById("root")).render(
