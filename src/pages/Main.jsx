b6d75be4e084fbedb8a5c03b41b1b274250d4ce7
kipkoechkelvin <mutaikevin41@gmail.com>
2025-06-17 16:44:23 +0300

    main.jsx

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,26 +1,26 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./styles/Home.css";
-import App from "./pages/Home.jsx";
-import { HelmetProvider } from 'react-helmet-async';
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+// import "./styles/Home.css";
+// import App from "./pages/Home.jsx";
+// import { HelmetProvider } from 'react-helmet-async';
+// import { BrowserRouter } from "react-router-dom";
+// import "./index.css";
 
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <HelmetProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </HelmetProvider>
-  </React.StrictMode>
-);
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// );
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <HelmetProvider>
+//       <BrowserRouter>
+//         <App />
+//       </BrowserRouter>
+//     </HelmetProvider>
+//   </React.StrictMode>
+// );
 
-<script type="module" src="/src/main.js"></script>
+// <script type="module" src="/src/main.js"></script>
 
